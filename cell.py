0dad4a88e7804fec072edefec6197ccb8005e29f
VLa24 <123402491+SVA44@users.noreply.github.com>
2023-11-04 16:58:08 +0700

    Add files via upload

--- cell.py
+++ cell.py
@@ -20,3 +20,31 @@ class Cell:
             pygame.draw.line(sc, pygame.Color('darkgreen'), (x + tile, y + tile),(x, y + tile), self.thickness)
         if self['left']:
             pygame.draw.line(sc, pygame.Color('darkgreen'), (x, y + tile), (x, y), self.thickness)
+    
+    # check if cell does exist and returns it if it does
+    def check_cell(self, x, y, cols, rows, grid_cells):
+        find_index = lambda x,y: x + y * cols
+        if x < 0 or x > cols - 1 or y < 0  or y > rows - 1:
+            return False
+        return grid_cells[find_index(x, y)]
+    
+    # checking cell neighbors of current cell if visited (carved) or not
+    def check_neighbors(self, cols, rows, grid_cells):
+        neighbors = []
+        top = self.check_cell(self.x, self.y - 1, cols, rows, grid_cells)
+        right = self.check_cell(self.x + 1, self.y, cols, rows, grid_cells)
+        bottom = self.check_cell(self.x, self.y + 1, cols, rows, grid_cells)
+        left = self.check_cell(self.x - 1, self.y, cols, rows, grid_cells)
+        
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+        if left and not left.visited:
+            neighbors.append(left)
+        return choice(neighbors) if neighbors else False
+    
+    
+
